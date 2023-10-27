acb89a05d52982303d043ac6b22db5be5df9d777
ganebe <Lok>
2023-10-26 23:13:17 -0700

    updated version

--- graphm.h
+++ graphm.h
@@ -1,5 +1,9 @@
 #ifndef GRAPHM_H
 #define GRAPHM_H
+#include "nodedata.h"
+
+const int MAXNODES = 100;
+
 
 class GraphM{
 
@@ -7,6 +11,16 @@ public:
 
 private:
 
+    struct TableType {
+        bool visited; // whether node has been visited
+        int dist; // shortest distance from source known so far
+        int path; // previous node in path of min dist
+        };
+
+    NodeData data[MAXNODES]; // data for graph nodes
+    int C[MAXNODES][MAXNODES]; // Cost array, the adjacency matrix
+    int size; // number of nodes in the graph
+    TableType T[MAXNODES][MAXNODES]; // stores visited, distance, path
 };
 
 #endif
