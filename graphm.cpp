00a896659e8ec1eae1fdb58b191a1daad841ea8f
Vince <Vinceng13@gmail.com>
2023-11-01 18:44:49 -0700

    Default constructor

--- graphm.cpp
+++ graphm.cpp
@@ -2,5 +2,19 @@
 
 GraphM::GraphM()
 {
-    
+    size = 0;
+    for (int i = 1; i < MAXNODES; i++)
+    {
+        for (int j = 1; j < MAXNODES; j++)
+        {
+            C[i][j] = 0;
+            T[i][j].visited = 0;
+            T[i][j].dist = 0;
+            T[i][j].path = 0;
+        }
+    }
 }
+
+
+
+
