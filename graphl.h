200810007a7b75771dd88deed1b7cb88fa167ddc
ganebe <Lok>
2023-11-06 22:37:41 -0800

    added comments

--- graphl.h
+++ graphl.h
@@ -8,35 +8,50 @@
 using namespace std;
 
 
+// ------------------------------------------------GraphL.h -------------------------------------------------------
+// CSS 343 B Au 23
+// Created on 11/6/2023
+// Last Modified on 11/6/2023
+// Created by Vince Nguyen and Lok Yin Wong 
+// --------------------------------------------------------------------------------------------------------------------
+// This header file contains the class GraphM. The class has a function that reads a file to build a graph using adjacency
+// list. Once the graph is builed, the user can use displayGraph and depth firstSearch to display the graph differently. 
+// --------------------------------------------------------------------------------------------------------------------
+// The class should be able to build and display the graph from a file that has correct format and vaild input
+// --------------------------------------------------------------------------------------------------------------------
+
 
 class GraphL{
 
 public:
-GraphL();
-~GraphL();
+//constructors and destructor
+GraphL();   //deafult constructor to initialize all variables
+~GraphL();  //destructor to free memroy that was used
 
-void buildGraph(ifstream &);
-void displayGraph();
-void depthFirstSearch();
+//class functions
+void buildGraph(ifstream &);    //builds a graph using the information provided in the file
+void displayGraph();            //display the graph alphabetically
+void depthFirstSearch();        //display the order of dfs result of the graph
 
 private:
-static const int MAXNODES = 101;
+static const int MAXNODES = 101;    //Maxmium of nodes this class can handles
 struct EdgeNode {
     int adjGraphNode; // subscript of the adjacent graph node
-    EdgeNode* nextEdge;
+    EdgeNode* nextEdge; //pointer to the next edge node
 };
 
-struct GraphNode { // structs used for simplicity, use classes if desired
+struct GraphNode { // structs used for simplicity
     EdgeNode* edgeHead; // head of the list of edges
     NodeData* data; // data information about each node
     bool visited;
 };
 
-GraphNode* arr_;
-int number_of_edge_node_;
-void makeEmpty();
-void makeEmptyHelper(EdgeNode *&);
-void depthFirstSearchHelper(GraphNode &, int);
+GraphNode* arr_;    //array that contains all the GraphNode
+int number_of_graph_node_; //number of nodes in the graph
+//helper functions
+void makeEmpty();   //reset the graph and free memeory
+void makeEmptyHelper(EdgeNode *&);  //helper for makeEmpty()
+void depthFirstSearchHelper(GraphNode &, int);  //helper for dfs
 };
 
 #endif
